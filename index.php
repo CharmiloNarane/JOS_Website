30691a9de5e800509e3800fd1d2ef3694ef20a80
HomieWilliam <32295997+HomieWilliam@users.noreply.github.com>
2017-12-17 12:36:53 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,139 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>JOS Eventos</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css"> 
+    
+    <!--// css -->
+    <link rel="stylesheet" type="text/css" href="Css/Desktop.css" media="(min-width:940px)">       
+    <link rel="stylesheet" type="text/css" href="Css/mobile.css" media="(max-width:520px)">
+      
+    
+    <!-- font -->
+    <link href="Css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all" />
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">  
+      
+    <!-- Cloud -->
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>  
+      
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>  
+    <script type="text/javascript" src="Js/Animacoes.js"></script>
+      
+  </head>
+    
+  <body id="body" data-spy="scroll" data-target=".header"> 
+        
+       <!-- Cabecalho da pagina -->
+       <?php include("Itens/header.php");?>
+      
+      <!-- Carousel da pagina -->
+       <?php include("Itens/carousel.php");?> 
+      
+      <!-- Inicio da seccao do corpo da pagina -->
+       <section class="corpo">
+            <!-- Inicio da area sobre nos -->
+           <div class="SobreNos">
+               <div class="SobreNosCentro">
+                   <div> <a href="#"><i class="fa fa-facebook" id="grayI" aria-hidden="true"></i></a> </div>
+                   
+                   <div>  </div>
+                   <div> </div>
+                   <div><a href="#"><i class="fa fa-twitter" id="grayI" aria-hidden="true"></i></a> </div>
+               </div>
+               
+               <h1 class="Quem">Quem somos?</h1>
+               
+               <div class="AreaDeTexto">
+                   <p>bvkjdf dfgvkjdfkjvd dksfj d djfksdfsdbkfjbsdbfsdf dfksdjfksjdkfsd fsdjfbskdbvksjdfs df skdvskbvksndfcskd vsdjvbkdfvbdkfbvkdbvsdbkv sdkvbskdbvksdvdbvksdkvbkjdbkvbs sdvjbsdv sdjvksdv sdjvj vsdv s vdjvksjdbvksdjv sdkjvksjv sdjvsdvvjskdv sdvjksdv dkjvd kzkkdvdk vksjsksjdffnwke cksdksj dckcsdvks dkv sdjvksdjfj vdjvdkjfv jdkdsvvjdfdjf djfskdj sdjfdj d dj vdsm\. dfsdnksd sdjnfskjdffsjdfsd,sdfjsdkjf sd.</p>
+               </div>
+               
+               <a href="" class="LerMais">Ler Mais</a>
+           </div> <!-- Fim da area sobre nos/Quem somos -->
+               
+            <!-- Inicio da area de projectos -->
+           <div class="Projetos">
+               <div class="DivLateral">
+                   <div class="circL">
+                       <span class="gly glyphicon glyphicon-chevron-left"></span>
+                   </div>
+               </div>
+               
+               <div class="ProjectoCentro">
+                   
+                   <div class="BoxProjecto">
+                       <div class="foto">
+                            <img src="">
+                       </div> 
+                       
+                       <div class="ProjetoInfo">
+                           <h1 class="Id">Bar</h1>
+                           <p>hvjhdbfjkdv</p>
+                       </div>
+                   </div> 
+                   
+                    <div class="BoxProjecto">
+                       <div class="foto">
+                            <img src="">
+                       </div> 
+                       
+                       <div class="ProjetoInfo">
+                           <h1 class="Id">Bar</h1>
+                           <p>hvjhdbfjkdv</p>
+                       </div>
+                   </div> 
+                   
+                    <div class="BoxProjecto">
+                       <div class="foto">
+                            <img src="">
+                       </div> 
+                       
+                       <div class="ProjetoInfo">
+                           <h1 class="Id">Bar</h1>
+                           <p>hvjhdbfjkdv</p>
+                       </div>
+                   </div> 
+                   
+               </div>
+               
+               
+               <div class="DivLateral">
+                    <div class="circR">
+                   <span class="gly glyphicon glyphicon-chevron-right"></span>
+                   </div>
+               </div>
+           </div> <!-- Fim da area de projectos -->
+           
+            <!-- Inicio da area de nossos servicos -->
+           <div class="Servicos">
+               <a href=""><div>
+                   <div class="circulo"></div>
+                   <div><h1 class="servicoTitulo">Titulo</h1></div>
+                   <div class="TextoServico"><p>dklvlkdflkvdlkfnbdfkdvn  d ddkjdfv sdjvsdvvjskdv sdvjksdv dkjvd kzkkdvdk vks kdfjsdsdmvs d vdf</p></div>
+               </div></a>
+               
+               <a href=""><div></div></a>
+               <a href=""><div></div></a>
+           </div> <!-- Fim da area de servicos -->
+           
+       </section> <!-- Fim da seccao do corpo da pagina -->
+      
+      
+       <?php include("Itens/footer.php");?>
+     
+
+        
+         
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      
+      <script src="Js/jquery.js" type="text/javascript"></script>
+      <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script> 
+  </body>
+</html>
