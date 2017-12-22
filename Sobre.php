84461f73985c5a58c098d117e46c8a499408ae12
CharmiloNarane <34478715+CharmiloNarane@users.noreply.github.com>
2017-12-22 11:09:17 -0500

    Create Sobre.php

---
+++ Sobre.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    
+ <meta charset="utf-8"/>    
+ <title> Sobre Nos</title>  
+    
+    <!-- Aplicacao do Bootstrap na Pagina Sobre Nos -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <!-- Jquery -->
+    <script src="bootstrap/js/jquery.js"></script>
+    
+<script src="bootstrap/js/bootstrap.js"></script> 
+    <!-- Fim da aplicacao do Bootstrap -->
+</head>  
+    
+<body>
+        <!-- Todo conentor e fluido e responsivo ao browser desktop -->
+ <div class="container-fluid"> 
+    
+     
+     
+  <div class="row">  <!-- Header -->
+    <div class="col-md-12" style="background:black; color:aliceblue;" ><h3> J.O.S Eventos</h3></div>     
+
+        <!-- Cabecalho da pagina, que contem o menu -->
+      <!--   <h3 style="color:darkgray; background-color:black; font-family: inherit;">J.O.S Eventos</h3> -->     
+      <nav class="navbar navbar-default navbar-fixed-top">
+          <div class="container">
+          <div class="navbar-header">  
+              <a class="navbar-brand" href="#">Bootstrap</a>
+              </div>
+              <div> 
+              
+                  <ul class="nav navbar-nav">
+            <li><a href="#">HOME</a></li>  
+            <li class="active"><a href="#">SOBRE NOS</a></li>
+            <li><a href="#">PORTFOLIO</a></li>
+            <li><a href="#">PRODUCTOS E SERVICOS</a></li>
+            <li><a href="#">CONTACTE-NOS</a></li>
+                  </ul>
+              
+              
+              </div>
+          </div>
+      
+      </nav>
+
+    
+            
+    </div>   
+        
+        
+    
+
+     
+    <div class="jumbotron" style="background-color:antiquewhite;">  <h4>Sobre Nos </h4>  </div>
+      
+<h3><small>Quem Somos?</small></h3> 
+     <br>
+     <p align="center"> A <abbr title="Jos Eventos Lda"><mark>J.O.S</mark></abbr> E uma empresa pioneira no ramo de entreterimento, actuando a mais de 3 anos no mercado nacional e internacional. Sempre transmitindo cofianca, transparencia e dinamiso como sinonimo de nossos trabalho. </p>
+     
+     <br>
+<div id="Servicos" class="jumbotron"  "row"  style="background-color:#320006; color:white;">
+       <h3 align="center"><small>Nossos Servicos</small></h3>
+       <figcaption><strong>Bar</strong></figcaption>  <img src="bootstrap/imagens/Sobre/BEER.png">
+     
+     </div>     
+     
+     
+ </div>
+</body>
+</html>
